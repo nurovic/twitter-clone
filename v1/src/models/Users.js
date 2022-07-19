bd40bd02410cebfb87e18c7b3c90575103be5f60
nurovic <mertnurovic@gmail.com>
2022-07-20 00:46:29 +0300

    Added new feature

--- v1/src/models/Users.js
+++ v1/src/models/Users.js
@@ -3,9 +3,15 @@ const Mongoose = require("mongoose");
 const UserSchema = new Mongoose.Schema(
   {
     full_name: String,
+    nick_name: String,
     password: String,
     email: String,
-
+    profil_picture: String,
+    tweets: [],
+    replies: [],
+    followers: [],
+    following: [],
+    likes: []
   },
   { timestamps: true, versionKey: false }
 );
