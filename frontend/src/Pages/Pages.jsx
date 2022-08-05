b194173397776a27d673f3f137bfc6d333e3eb8a
nurovic <mertnurovic@gmail.com>
2022-08-05 16:45:27 +0300

    Added Profile Page

--- frontend/src/Pages/Pages.jsx
+++ frontend/src/Pages/Pages.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Home from './Home'
+import Profile from './Profile'
 import { Route, Routes } from "react-router-dom";
 
 const Pages = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 };
